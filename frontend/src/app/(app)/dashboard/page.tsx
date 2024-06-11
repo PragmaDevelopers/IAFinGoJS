a45f25dd9bbfcb69d539d5bb44d8c2adb057d5b2
Lucas Emanuel Santana Dos Santos <lucasemanuel2077@gmail.com>
2024-06-11 15:13:07 -0300

    posicao da sidebar consertada

--- frontend/src/app/(app)/dashboard/page.tsx
+++ frontend/src/app/(app)/dashboard/page.tsx
@@ -2,8 +2,8 @@ import { ReactElement } from "react";
 
 export default function Page(): ReactElement<any, any> {
     return (
-        <main>
-            <h1>Dashboard page</h1>
+        <main className="justify-center flex">
+            <h1 className="text-black">Dashboard page</h1>
         </main>
     );
 }
--- frontend/src/components/clientside/ui/Sidebar.tsx
+++ frontend/src/components/clientside/ui/Sidebar.tsx
@@ -1,62 +1,69 @@
 "use client";
 
-import { ArrowBigRightDash, Coins, Contact, File, FileArchive, Home, Info, LayoutDashboard, LucideTrendingDown, LucideTrendingUp, ShowerHead, SidebarClose, X } from 'lucide-react';
+import { ArrowBigRightDash, Coins, File, Home, LayoutDashboard, LucideTrendingDown, LucideTrendingUp, X } from 'lucide-react';
 import React, { useState } from 'react';
+import { cn } from "@/lib/utils"
 
-function Sidebar() {
+const Sidebar: React.FC = () => {
     const [showSidebar, setShowSidebar] = useState(true);
-    return (
-        <div className="relative h-full">
-        <div
-            className={`h-full w-64 bg-gray-800 text-white fixed top-0 transition-transform duration-300 ${
-                showSidebar ? 'translate-x-0' : '-translate-x-64'
-            }`}
-        >
-            <div className="p-4">
-                            <div className="flex justify-between">
-                                <h1 className="text-2xl font-bold">IA Finanças</h1>
-                                <button type="button" onClick={() => setShowSidebar(prev => !prev)}><X /></button>
-                            </div>
-                            <div className="flex gap-6 justify-center mt-4">
-                                <a href="#" className="w-16 h-16 p-5 bg-green-600">
-                                    <LucideTrendingUp />
-                                </a>
-                                <a href="#" className="w-16 h-16 p-5 bg-red-600">
-                                    <LucideTrendingDown />
-                                </a>
-                            </div>
-                            <ul className="mt-4">
-                                <li className="mb-2 flex items-center">
-                                    <Home className="mr-2" />
-                                    <a href="/home" className="block p-2 hover:bg-gray-700 rounded">Home</a>
-                                </li>
-                                <li className="mb-2 flex items-center">
-                                    <LayoutDashboard className="mr-2" />
-                                    <a href="/dashboard" className="block p-2 hover:bg-gray-700 rounded">Dashboard</a>
-                                </li>
-                                <li className="mb-2 flex items-center">
-                                    <File className="mr-2" />
-                                    <a href="/extrato" className="block p-2 hover:bg-gray-700 rounded">Extrato</a>
-                                </li>
-                                <li className="mb-2 flex items-center">
-                                    <Coins className="mr-2" />
-                                    <a href="#" className="block p-2 hover:bg-gray-700 rounded">IA Finanças</a>
-                                </li>
-                            </ul>
-                        </div>
-        </div>
 
-        {!showSidebar && (
-            <button
-                onClick={() => setShowSidebar(true)}
-                type="button"
-                className="h-12 w-12 p-3 bg-gray-800 text-white fixed -left-2 hover:translate-x-2 transition-transform duration-300"
+    return (
+        <div className="h-full fixed">
+            <div
+                className={cn(
+                    "h-full w-64 bg-gray-800 text-white fixed top-0 transition-transform duration-300",
+                    {
+                        'translate-x-0': showSidebar,
+                        '-translate-x-64': !showSidebar,
+                    }
+                )}
             >
-                <ArrowBigRightDash />
-            </button>
-        )}
+                <div className="p-4">
+                    <div className="flex justify-between">
+                        <h1 className="text-2xl font-bold">IA Finanças</h1>
+                        <button type="button" onClick={() => setShowSidebar(prev => !prev)}>
+                            <X />
+                        </button>
+                    </div>
+                    <div className="flex gap-6 justify-center mt-4">
+                        <a href="#" className="w-16 h-16 p-5 bg-green-600">
+                            <LucideTrendingUp />
+                        </a>
+                        <a href="#" className="w-16 h-16 p-5 bg-red-600">
+                            <LucideTrendingDown />
+                        </a>
+                    </div>
+                    <ul className="mt-4">
+                        <li className="mb-2 flex items-center">
+                            <Home className="mr-2" />
+                            <a href="/home" className="block p-2 hover:bg-gray-700 rounded">Home</a>
+                        </li>
+                        <li className="mb-2 flex items-center">
+                            <LayoutDashboard className="mr-2" />
+                            <a href="/dashboard" className="block p-2 hover:bg-gray-700 rounded">Dashboard</a>
+                        </li>
+                        <li className="mb-2 flex items-center">
+                            <File className="mr-2" />
+                            <a href="/extrato" className="block p-2 hover:bg-gray-700 rounded">Extrato</a>
+                        </li>
+                        <li className="mb-2 flex items-center">
+                            <Coins className="mr-2" />
+                            <a href="#" className="block p-2 hover:bg-gray-700 rounded">IA Finanças</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            {!showSidebar && (
+                <button
+                    onClick={() => setShowSidebar(true)}
+                    type="button"
+                    className="h-12 w-12 p-3 bg-gray-800 text-white fixed top-4 -left-2 hover:translate-x-2 transition-transform duration-300"
+                >
+                    <ArrowBigRightDash />
+                </button>
+            )}
         </div>
-    )
+    );
 };
 
 export default Sidebar;
